75398600e84e067ddcf60f501b4c6e900abe3e4c
hai <hai.sok@wingmoney.com>
2022-07-23 15:09:55 +0700

    add child page

--- page.php
+++ page.php
@@ -14,11 +14,19 @@
     </div>
 
     <div class="container container--narrow page-section">
-      <div class="metabox metabox--position-up metabox--with-home-link">
-        <p>
-          <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
-        </p>
-      </div>
+      <?php 
+        $parentId = wp_get_post_parent_id(get_the_ID());
+        if($parentId) { ?>
+          <div class="metabox metabox--position-up metabox--with-home-link">
+            <p>
+              <a class="metabox__blog-home-link" href="<?= get_permalink($parentId) ?>">
+                <i class="fa fa-home" aria-hidden="true"></i> Back to <?= get_the_title($parentId) ?>
+              </a> 
+              <span class="metabox__main"><?php the_title(); ?></span>
+            </p>
+          </div>
+        <?php }
+      ?>
 
       <!-- <div class="page-links">
         <h2 class="page-links__title"><a href="#">About Us</a></h2>
