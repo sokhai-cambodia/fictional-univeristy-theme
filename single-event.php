3b0047e7e99ae4a4f652199d388377e34e2c4787
hai <hai.sok@wingmoney.com>
2022-08-17 12:06:51 +0700

    add related_program list on single_events

--- single-event.php
+++ single-event.php
@@ -20,7 +20,20 @@
       </div>
 
       <div class="generic-content"><?php the_content(); ?></div>
-
+      <?php 
+        $related_programs = get_field('related_programs');
+        if($related_programs) {
+          echo "<h3>Related Programs</h3>";
+          echo "<ul class='link-list min-list'>";
+          foreach($related_programs as $related_program) { ?>
+            <li>
+                <a href="<?php echo get_the_permalink($related_program); ?>"><?php echo get_the_title($related_program); ?></a>
+            </li>
+        <?php 
+          } 
+          echo "</ul>";
+        }
+      ?>
     </div>
     
   <?php }
