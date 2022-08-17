110b8b93cd3647df8939f9b2083a63b80befd2d8
hai <hai.sok@wingmoney.com>
2022-08-17 12:11:20 +0700

    add upcomming event on programs

--- single-program.php
+++ single-program.php
@@ -20,7 +20,55 @@
       </div>
 
       <div class="generic-content"><?php the_content(); ?></div>
-
+        <?php
+          $today = date('Ymd');
+          $upcomming_events = new WP_Query(array(
+            'posts_per_page' => 2,
+            'post_type' => 'event',
+            'meta_key' => 'event_date',
+            'orderby' => 'meta_value_num',
+            'order' => 'ASC',
+            'meta_query' => array(
+              array(
+                'key' => 'event_date',
+                'compare' => '>=',
+                'value' => $today,
+                'type' => 'numeric'
+              ),
+              array(
+                'key' => 'related_programs',
+                'compare' => 'LIKE',
+                'value' => strval(get_the_ID()),
+              )
+            )
+          ));
+          if($upcomming_events->have_posts()) {
+            echo '<h2 class="headline headline--small-plus t-center">Upcoming Events</h2>';
+            while ( $upcomming_events->have_posts() ) {
+              $upcomming_events->the_post();?>
+              <div class="event-summary">
+                <a class="event-summary__date t-center" href="#">
+                  <span class="event-summary__month">Mar</span>
+                  <span class="event-summary__day">25</span>  
+                </a>
+                <div class="event-summary__content">
+                  <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                  <p>
+                    <?php 
+                      if (has_excerpt()) {
+                        echo get_the_excerpt();
+                      } else {
+                        echo wp_trim_words(get_the_content(), 18);
+                      } 
+                    ?>
+                    <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a>
+                  </p>
+                </div>
+              </div>
+            <?php
+            }
+          }
+        ?>
     </div>
     
   <?php }
